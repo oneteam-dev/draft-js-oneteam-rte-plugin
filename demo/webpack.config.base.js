73a1a5115226b080dedb5f7413ae208c1f099131
sugarshin <shinsugar@gmail.com>
2017-05-24 15:43:43 +0900

    Update modules + Use yarn

--- demo/webpack.config.base.js
+++ demo/webpack.config.base.js
@@ -1,37 +1,45 @@
 var path = require('path'); // eslint-disable-line no-var
 var ExtractTextPlugin = require('extract-text-webpack-plugin'); // eslint-disable-line no-var
-var autoprefixer = require('autoprefixer'); // eslint-disable-line no-var
 
 module.exports = {
   resolve: {
     alias: {
       'draft-js-oneteam-rte-plugin': path.join(__dirname, '..', 'src'),
       react: path.join(__dirname, '..', 'node_modules', 'react'),
-    },
-    extensions: ['', '.js'],
+    }
   },
   module: {
-    loaders: [
+    rules: [
       {
         // match all js files except example.js
         test: /^(?!.*example\.js$).*\.js$/,
-        loaders: ['babel'],
+        loaders: 'babel-loader',
         exclude: /node_modules/,
         include: path.join(__dirname, 'client'),
-      }, {
+      },
+      {
         test: /\.js$/,
-        loaders: ['babel'],
+        loaders: 'babel-loader',
         include: [
           path.join(__dirname),
           path.join(__dirname, '..', 'src'),
           path.join(__dirname, 'client')
         ],
-      }, {
+      },
+      {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[local]___[hash:base64:5]!postcss-loader'),
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: { modules: true, importLoaders: 1, localIdentName: '[local]___[hash:base64:5]' }
+            },
+            { loader: 'postcss-loader' }
+          ]
+        }),
         include: path.join(__dirname, 'client', 'components'),
       }
     ],
-  },
-  postcss: [autoprefixer({ browsers: ['> 1%'] })],
+  }
 };
